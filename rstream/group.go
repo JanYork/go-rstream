a8cc14ea9cbf3279f2445badb69a4abd1be35078
JanYork <747945307@qq.com>
2024-07-09 13:32:09 +0800

    REFACTOR The group module was refactored, the Group structure was added, and the Group-related methods were migrated from the GroupManager to the Group 2.

--- rstream/group.go
+++ rstream/group.go
@@ -5,20 +5,22 @@ import (
 	"errors"
 	"github.com/go-redis/redis/v8"
 	"github.com/rs/zerolog/log"
+	"time"
 )
 
-// GroupManager handles the creation and management of consumer groups
 type GroupManager struct {
-	client             *redis.Client
 	queue              *Queue
 	destroyMovePending bool // 注销组时是否将未处理的消息转移到死信队列
+	groups             map[string]*Group
+	deadLetterGroups   map[string]*Group
 }
 
-// NewGroupManager creates a new GroupManager
-func NewGroupManager(client *redis.Client, queue *Queue) *GroupManager {
+// NewGroupManager Creates a new GroupManager
+func NewGroupManager(queue *Queue) *GroupManager {
 	return &GroupManager{
-		client: client,
-		queue:  queue,
+		queue:            queue,
+		groups:           make(map[string]*Group),
+		deadLetterGroups: make(map[string]*Group),
 	}
 }
 
@@ -26,11 +28,11 @@ func (g *GroupManager) SwitchDestroyMovePending() {
 	g.destroyMovePending = !g.destroyMovePending
 }
 
-// CreateGroup creates a consumer group if it does not already exist
-func (g *GroupManager) CreateGroup(ctx context.Context, group string, start string) error {
+// CreateGroup Creates a consumer group if it does not already exist
+func (g *GroupManager) CreateGroup(ctx context.Context, groupName string, start string) error {
 	stream := buildStreamKey(g.queue.Name)
 
-	log.Debug().Msgf("Creating group %s, start: %s, stream: %s", group, start, stream)
+	log.Debug().Msgf("Creating groupName %s, start: %s, stream: %s", groupName, start, stream)
 
 	// Check if it meets the queue configuration rules
 	if err := g.checkMaxGroups(ctx, stream); err != nil {
@@ -41,156 +43,90 @@ func (g *GroupManager) CreateGroup(ctx context.Context, group string, start stri
 		start = "$"
 	}
 
-	// Check if the group already exists
-	_, err := g.client.XGroupCreateMkStream(ctx, stream, group, start).Result()
+	group := &Group{
+		Name:      groupName,
+		Queue:     g.queue,
+		CreatedAt: time.Now(),
+	}
+
+	// Check if the groupName already exists
+	_, err := g.queue.Client.XGroupCreateMkStream(ctx, stream, group.Name, start).Result()
 	if err != nil {
 		if err.Error() == "BUSYGROUP Consumer Group name already exists" {
-			// The group already exists, no need to create it again
+			// The groupName already exists, no need to create it again
 			return nil
 		}
-		log.Error().Err(err).Msgf("Failed to create group %s", group)
+		log.Error().Err(err).Msgf("Failed to create groupName %s", group.Name)
 		return err
 	}
+
+	g.groups[groupName] = group
+
 	return nil
 }
 
-// CreateDeadLetterGroup creates a dead letter consumer group if it does not already exist
-func (g *GroupManager) CreateDeadLetterGroup(ctx context.Context, group string, start string) error {
-	deadLetterStream := buildDeadLetterKey(g.queue.DeadLetterName)
-
-	log.Debug().Msgf("Creating dead letter group %s, start: %s, stream: %s", group, start, deadLetterStream)
-
-	if start == "" {
-		start = "$"
+// CreateDeadLetterGroup Creates a dead letter consumer group if it does not already exist
+func (g *GroupManager) CreateDeadLetterGroup(ctx context.Context, groupName string) error {
+	stream := buildStreamKey(g.queue.DeadLetterName)
+	group := &Group{
+		Name:      groupName,
+		Queue:     g.queue,
+		CreatedAt: time.Now(),
 	}
 
-	// Check if the group already exists
-	_, err := g.client.XGroupCreateMkStream(ctx, deadLetterStream, group, start).Result()
+	// Check if the groupName already exists
+	_, err := g.queue.Client.XGroupCreateMkStream(ctx, stream, group.Name, "$").Result()
 	if err != nil {
 		if err.Error() == "BUSYGROUP Consumer Group name already exists" {
-			// The group already exists, no need to create it again
+			// The groupName already exists, no need to create it again
 			return nil
 		}
-		log.Error().Err(err).Msgf("Failed to create dead letter group %s", group)
+		log.Error().Err(err).Msgf("Failed to create groupName %s", group.Name)
 		return err
 	}
+
+	g.deadLetterGroups[groupName] = group
+
 	return nil
 }
 
-// DestroyGroup removes a consumer group, and returns the number of pending messages, the number of consumers, and the group's next offset
-func (g *GroupManager) DestroyGroup(ctx context.Context, group string) (int64, int64, string, error) {
-	stream := buildStreamKey(g.queue.Name)
+// CreateGroupWithDeadLetter Creates a consumer group and a dead letter consumer group if they do not already exist
+func (g *GroupManager) CreateGroupWithDeadLetter(ctx context.Context, groupName string, start string) error {
+	err := g.CreateGroup(ctx, groupName, start)
+	if err != nil {
+		log.Error().Err(err).Msgf("Failed to create groupName %s", groupName)
+		return err
+	}
 
-	log.Debug().Msgf("Removing group %s, stream: %s", group, stream)
+	err = g.CreateDeadLetterGroup(ctx, groupName)
+	if err != nil {
+		log.Error().Err(err).Msgf("Failed to create dead letter groupName %s", groupName)
+		return err
+	}
+
+	return nil
+}
+
+// DestroyGroup Removes a consumer group, and returns the number of pending messages, the number of consumers, and the group's next offset
+func (g *GroupManager) DestroyGroup(ctx context.Context, groupName string) (int64, int64, string, error) {
+	group, exists := g.groups[groupName]
+	if !exists {
+		return 0, 0, "", errors.New("group does not exist")
+	}
 
 	// Get the number of pending messages
-	pending, err := g.client.XPending(ctx, stream, group).Result()
+	pendingCount, consumersCount, nextOffset, err := group.Destroy(ctx, g.destroyMovePending)
 	if err != nil {
-		logError("Failed to get pending messages for group %s", group, err)
 		return 0, 0, "", err
 	}
 
-	// Get the number of consumers
-	consumers, err := g.client.XInfoConsumers(ctx, stream, group).Result()
-	if err != nil {
-		logError("Failed to get consumers for group %s", group, err)
-		return 0, 0, "", err
-	}
+	delete(g.groups, groupName)
 
-	// Get the group's next offset
-	nextOffset, err := g.getGroupNextOffset(ctx, stream, group)
-	if err != nil {
-		logError("Failed to get next offset for group %s", group, err)
-		return 0, 0, "", err
-	}
-
-	// If enabled, move pending messages to the dead letter queue
-	if g.destroyMovePending {
-		err := g.movePendingMessagesToDeadLetter(ctx, stream, group, pending.Count)
-		if err != nil {
-			return 0, 0, "", err
-		}
-	}
-
-	// Destroy the group
-	_, err = g.client.XGroupDestroy(ctx, stream, group).Result()
-	if err != nil {
-		logError("Failed to remove group %s", group, err)
-		return 0, 0, "", err
-	}
-
-	return pending.Count, int64(len(consumers)), nextOffset, nil
-}
-
-func (g *GroupManager) getGroupNextOffset(ctx context.Context, stream, group string) (string, error) {
-	groupInfoList, err := g.client.XInfoGroups(ctx, stream).Result()
-	if err != nil {
-		return "", err
-	}
-
-	for _, groupInfo := range groupInfoList {
-		if groupInfo.Name == group {
-			return groupInfo.LastDeliveredID, nil
-		}
-	}
-
-	return "", errors.New("failed to get next offset or group does not exist")
-}
-
-func (g *GroupManager) movePendingMessagesToDeadLetter(ctx context.Context, stream, group string, count int64) error {
-	pendingMsgs, err := g.client.XPendingExt(ctx, &redis.XPendingExtArgs{
-		Stream: stream,
-		Group:  group,
-		Start:  "-",
-		End:    "+",
-		Count:  count,
-	}).Result()
-	if err != nil {
-		logError("Failed to get pending messages for group %s", group, err)
-		return err
-	}
-
-	for _, pmsg := range pendingMsgs {
-		err := g.moveMessageToDeadLetter(ctx, stream, group, pmsg.ID)
-		if err != nil {
-			return err
-		}
-	}
-
-	return nil
-}
-
-func (g *GroupManager) moveMessageToDeadLetter(ctx context.Context, stream, group, msgID string) error {
-	msgDetails, err := g.client.XRange(ctx, stream, msgID, msgID).Result()
-	if err != nil {
-		logError("Failed to get message details for %s", msgID, err)
-		return err
-	}
-
-	if len(msgDetails) != 1 {
-		return errors.New("unexpected number of messages returned")
-	}
-
-	_, err = g.client.XAdd(ctx, &redis.XAddArgs{
-		Stream: buildDeadLetterKey(g.queue.DeadLetterName),
-		Values: msgDetails[0].Values,
-	}).Result()
-	if err != nil {
-		logError("Failed to add message %s to dead letter queue", msgID, err)
-		return err
-	}
-
-	_, err = g.client.XAck(ctx, stream, group, msgID).Result()
-	if err != nil {
-		logError("Failed to ACK message %s", msgID, err)
-		return err
-	}
-
-	return nil
+	return pendingCount, consumersCount, nextOffset, nil
 }
 
 func (g *GroupManager) checkMaxGroups(ctx context.Context, stream string) error {
-	groups, err := g.client.XInfoGroups(ctx, stream).Result()
+	groups, err := g.queue.Client.XInfoGroups(ctx, stream).Result()
 	if err != nil {
 		log.Error().Err(err).Msgf("Failed to get groups for stream %s", stream)
 		return err
@@ -205,6 +141,163 @@ func (g *GroupManager) checkMaxGroups(ctx context.Context, stream string) error 
 	return nil
 }
 
+type Group struct {
+	Name      string
+	Queue     *Queue
+	CreatedAt time.Time
+}
+
+// Destroy Removes the consumer group, optionally moving pending messages to the dead letter queue
+func (g *Group) Destroy(ctx context.Context, movePending bool) (int64, int64, string, error) {
+	streamKey := buildStreamKey(g.Queue.Name)
+
+	log.Debug().Msgf("Removing groupName %s, stream: %s", g.Name, streamKey)
+
+	// Get the number of pending messages
+	pending, err := g.Queue.Client.XPending(ctx, streamKey, g.Name).Result()
+	if err != nil {
+		logError("Failed to get pending messages for groupName %s", g.Name, err)
+		return 0, 0, "", err
+	}
+
+	// Get the number of consumers
+	consumers, err := g.Queue.Client.XInfoConsumers(ctx, streamKey, g.Name).Result()
+	if err != nil {
+		logError("Failed to get consumers for groupName %s", g.Name, err)
+		return 0, 0, "", err
+	}
+
+	// Get the groupName's next offset
+	nextOffset, err := g.getGroupNextOffset(ctx)
+	if err != nil {
+		logError("Failed to get next offset for groupName %s", g.Name, err)
+		return 0, 0, "", err
+	}
+
+	// If enabled, move pending messages to the dead letter queue
+	if movePending {
+		err := g.movePendingMessagesToDeadLetter(ctx, pending.Count)
+		if err != nil {
+			return 0, 0, "", err
+		}
+	}
+
+	// Destroy the groupName
+	_, err = g.Queue.Client.XGroupDestroy(ctx, streamKey, g.Name).Result()
+	if err != nil {
+		logError("Failed to remove groupName %s", g.Name, err)
+		return 0, 0, "", err
+	}
+
+	return pending.Count, int64(len(consumers)), nextOffset, nil
+}
+
+func (g *Group) getGroupNextOffset(ctx context.Context) (string, error) {
+	groupInfoList, err := g.Queue.Client.XInfoGroups(ctx, buildStreamKey(g.Queue.Name)).Result()
+	if err != nil {
+		return "", err
+	}
+
+	for _, groupInfo := range groupInfoList {
+		if groupInfo.Name == g.Name {
+			return groupInfo.LastDeliveredID, nil
+		}
+	}
+
+	return "", errors.New("failed to get next offset or groupName does not exist")
+}
+
+func (g *Group) movePendingMessagesToDeadLetter(ctx context.Context, count int64) error {
+	streamKey := buildStreamKey(g.Queue.Name)
+	pendingMsgs, err := g.Queue.Client.XPendingExt(ctx, &redis.XPendingExtArgs{
+		Stream: streamKey,
+		Group:  g.Name,
+		Start:  "-",
+		End:    "+",
+		Count:  count,
+	}).Result()
+	if err != nil {
+		logError("Failed to get pending messages for groupName %s", g.Name, err)
+		return err
+	}
+
+	for _, pmsg := range pendingMsgs {
+		err := g.moveMessageToDeadLetter(ctx, pmsg.ID)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (g *Group) moveMessageToDeadLetter(ctx context.Context, msgID string) error {
+	streamKey := buildStreamKey(g.Queue.Name)
+	msgDetails, err := g.Queue.Client.XRange(ctx, streamKey, msgID, msgID).Result()
+	if err != nil {
+		logError("Failed to get message details for %s", msgID, err)
+		return err
+	}
+
+	if len(msgDetails) != 1 {
+		return errors.New("unexpected number of messages returned")
+	}
+
+	_, err = g.Queue.Client.XAdd(ctx, &redis.XAddArgs{
+		Stream: buildDeadLetterKey(g.Queue.DeadLetterName, g.Name),
+		Values: msgDetails[0].Values,
+	}).Result()
+	if err != nil {
+		logError("Failed to add message %s to dead letter queue", msgID, err)
+		return err
+	}
+
+	_, err = g.Queue.Client.XAck(ctx, streamKey, g.Name, msgID).Result()
+	if err != nil {
+		logError("Failed to ACK message %s", msgID, err)
+		return err
+	}
+
+	return nil
+}
+
 func logError(message, group string, err error) {
 	log.Error().Err(err).Msgf(message, group)
+}
+
+// RunScanPendingMessages Pending messages are scanned on a regular basis to transfer messages that have timed out but are not processed to a dead-letter queue
+func (g *Group) RunScanPendingMessages(ctx context.Context) error {
+	streamKey := buildStreamKey(g.Queue.Name)
+	blockTime := g.Queue.BlockTime
+	thresholdTime := blockTime + 30*time.Second
+
+	ticker := time.NewTicker(1 * time.Minute)
+	defer ticker.Stop()
+
+	for {
+		select {
+		case <-ctx.Done():
+			return ctx.Err()
+		case <-ticker.C:
+			pendingMsgs, err := g.Queue.Client.XPendingExt(ctx, &redis.XPendingExtArgs{
+				Stream: streamKey,
+				Group:  g.Name,
+				Start:  "-",
+				End:    "+",
+				Idle:   thresholdTime,
+				Count:  100,
+			}).Result()
+			if err != nil {
+				logError("Failed to get pending messages for groupName %s", g.Name, err)
+				return err
+			}
+
+			for _, pmsg := range pendingMsgs {
+				err := g.moveMessageToDeadLetter(ctx, pmsg.ID)
+				if err != nil {
+					return err
+				}
+			}
+		}
+	}
 }
